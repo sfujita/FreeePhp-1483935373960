4f62777f030d160c4509eb3aa0381442e52324a1
syun_alien <next.s.bdf@gmail.com>
2017-01-10 00:47:48 +0900

    コールバックを切り出し

--- php/callback.php
+++ php/callback.php
@@ -1,33 +1,16 @@
 <?php
-// アプリ設定
-define('APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b');
-define('SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841');
-define('CALLBACK_URL', 'https://freeephp.mybluemix.net/php/callback.php');
+// (3) Token取得後の処理。各種APIの実行
+if (! is_null ( $token ['access_token'] )) {
+        $header = [
+            'Authorization: Bearer ' . $token['access_token'],
+        ];
 
-// アクセストークンの取得
-$params = http_build_query(
-array(
-'grant_type' => 'authorization_code',
-'client_id' => APP_ID,
-'client_secret' => SECRET,
-'code' => $_GET['code'], //認証用URLで取得したコード
-'redirect_uri' => CALLBACK_URL, ));
-$headers = array( "Content-type: application/x-www-form-urlencoded" );
+	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
+	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+	$jsonResult = curl_exec ( $curl );
+	$result = json_decode ( $jsonResult, true );
 
-//POST送信
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'https://api.freee.co.jp/oauth/token');
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_HEADER, true);
-
-$response = curl_exec($curl);
-$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-$header = substr($response, 0, $header_size);
-$body = substr($response, $header_size);
-$result = json_decode($body, true);
-
-$access_token = $result['access_token'];
-print_r($access_token); //テスト用に表示
+	var_dump ( "<br />取得した情報<br />" );
+	var_dump ( $result );
+}
--- php/sample.php
+++ php/sample.php
@@ -1,7 +1,7 @@
 <?php
 define ( 'APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b' );
 define ( 'APP_SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841' );
-define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/sample.php' );
+define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/callback.php' );
 // ※注意：APP_CALLBACKの値は、freeeのアプリケーション一覧に登録されている
 // コールバックURIと同じにする必要があります。
 
@@ -32,17 +32,17 @@ if (! empty ( $_GET ['code'] )) {
 }
 
 // (3) Token取得後の処理。各種APIの実行
-if (! is_null ( $token ['access_token'] )) {
-        $header = [
-            'Authorization: Bearer ' . $token['access_token'],
-        ];
+// if (! is_null ( $token ['access_token'] )) {
+//         $header = [
+//             'Authorization: Bearer ' . $token['access_token'],
+//         ];
 
-	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
-	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
-	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
-	$jsonResult = curl_exec ( $curl );
-	$result = json_decode ( $jsonResult, true );
+// 	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
+// 	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+// 	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+// 	$jsonResult = curl_exec ( $curl );
+// 	$result = json_decode ( $jsonResult, true );
 
-	var_dump ( "取得した情報" );
-	var_dump ( $result );
-}
+// 	var_dump ( "取得した情報<br />" );
+// 	var_dump ( $result );
+// }
