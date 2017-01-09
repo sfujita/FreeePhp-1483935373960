2005ab0bb58919dd074f7f9fa655cd743eefff3a
syun_alien <next.s.bdf@gmail.com>
2017-01-09 23:25:51 +0900

    サンプルをコミットしてみる

---
+++ php/sample.php
@@ -0,0 +1,47 @@
+<?php
+define ( 'APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b' );
+define ( 'APP_SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841' );
+define ( 'APP_CALLBACK', 'http://http://192.168.110.202//freee20161204/sample.php' );
+// ※注意：APP_CALLBACKの値は、freeeのアプリケーション一覧に登録されている
+// コールバックURIと同じにする必要があります。
+
+// (1) いちばん最初の処理。OAuth2の入り口。
+if (empty ( $_GET )) {
+	printf ( '<html><a href="%s?client_id=%s&redirect_uri=%s&response_type=code">認証開始</a></html>', 'https://secure.freee.co.jp/oauth/authorize', // 認証用
+APP_ID, urlencode ( APP_CALLBACK ) );
+}
+
+// (2) freeeで「許可する」が押されたあとに実行する処理
+if (! empty ( $_GET ['code'] )) {
+        $content = [
+            "code"          => $_GET['code'],
+            "grant_type"    => "authorization_code",
+            "client_id"     => APP_ID,
+            "client_secret" => APP_SECRET,
+            "redirect_uri"  => APP_CALLBACK,
+        ];
+
+	$curl = curl_init ( 'https://api.freee.co.jp/oauth/token.json' ); // 認証済みToken取得用
+	curl_setopt ( $curl, CURLOPT_POST, TRUE );
+	curl_setopt ( $curl, CURLOPT_POSTFIELDS, http_build_query ( $content ) );
+	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+	$jsonToken = curl_exec ( $curl );
+	$token = json_decode ( $jsonToken, true );
+
+	var_dump ( $token );
+}
+
+// (3) Token取得後の処理。各種APIの実行
+if (! is_null ( $token ['access_token'] )) {
+        $header = [
+            'Authorization: Bearer ' . $token['access_token'],
+        ];
+
+	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
+	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+	$jsonResult = curl_exec ( $curl );
+	$result = json_decode ( $jsonResult, true );
+
+	var_dump ( $result );
+}
