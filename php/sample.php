ad1686cc0f091a66ffb765ea94d1279aca51e9cf
syun_alien <next.s.bdf@gmail.com>
2017-01-10 00:53:36 +0900

    コールバック修正

--- php/sample.php
+++ php/sample.php
@@ -32,17 +32,17 @@ if (! empty ( $_GET ['code'] )) {
 }
 
 // (3) Token取得後の処理。各種APIの実行
-// if (! is_null ( $token ['access_token'] )) {
-//         $header = [
-//             'Authorization: Bearer ' . $token['access_token'],
-//         ];
+if (! is_null ( $token ['access_token'] )) {
+        $header = [
+            'Authorization: Bearer ' . $token['access_token'],
+        ];
 
-// 	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
-// 	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
-// 	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
-// 	$jsonResult = curl_exec ( $curl );
-// 	$result = json_decode ( $jsonResult, true );
+	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報
+	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+	$jsonResult = curl_exec ( $curl );
+	$result = json_decode ( $jsonResult, true );
 
-// 	var_dump ( "取得した情報<br />" );
-// 	var_dump ( $result );
-// }
+	var_dump ( "取得した情報<br />" );
+	var_dump ( $result );
+}
