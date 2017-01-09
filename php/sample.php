f1f070947c940f6a36a5c7bf7da83ac6cf777f2c
syun_alien <next.s.bdf@gmail.com>
2017-01-10 00:18:40 +0900

    コールバック修正

--- php/sample.php
+++ php/sample.php
@@ -1,7 +1,7 @@
 <?php
 define ( 'APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b' );
 define ( 'APP_SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841' );
-define ( 'APP_CALLBACK', 'http://http://192.168.110.202//freee20161204/sample.php' );
+define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/callback.php' );
 // ※注意：APP_CALLBACKの値は、freeeのアプリケーション一覧に登録されている
 // コールバックURIと同じにする必要があります。
 
