89da1c9f9b7f65d023c80d72322f837646816076
syun_alien <next.s.bdf@gmail.com>
2017-01-09 22:31:44 +0900

    サンプル通りに配置

--- php/callback.php
+++ php/callback.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-<script src="../js/jquery-1.9.1.min.js"></script>
-<script src="../js/main.js" type="text/javascript"></script>
-<script src="https://appsforoffice.microsoft.com/lib/1/hosted/Office.js"
-	type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="./css/main.css" />
-<script>
-	// 画面読み込み時に実行される(word)
-	Office.initialize = function(reason) {
-		$(document).ready(
-				function() {
-					Office.context.document.addHandlerAsync(
-							Office.EventType.DocumentSelectionChanged,
-							document_SelectionChanged);
-					$('#search').click(searchDataBase);
-				});
-	}
-</script>
-</head>
-<body>
-	<button id="search" disabled="disabled">開始</button>
-	<br />
-</body>
+<?php
+// アプリ設定
+define('APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b');
+define('SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841');
+define('CALLBACK_URL', 'https://freeephp.mybluemix.net/php/callback.php');
 
-</html>
+// アクセストークンの取得
+$params = http_build_query(
+array(
+'grant_type' => 'authorization_code',
+'client_id' => APP_ID,
+'client_secret' => SECRET,
+'code' => $_GET['code'], //認証用URLで取得したコード
+'redirect_uri' => CALLBACK_URL, ));
+$headers = array( "Content-type: application/x-www-form-urlencoded" );
+
+//POST送信
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://api.freee.co.jp/oauth/token');
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HEADER, true);
+
+$response = curl_exec($curl);
+$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+$result = json_decode($body, true);
+
+$access_token = $result['access_token'];
+print_r($access_token); //テスト用に表示
--- php/main.php
+++ php/main.php
@@ -1,4 +1,16 @@
 <?php
+define('APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b');
+define('CALLBACK_URL', 'https://freeephp.mybluemix.net/php/callback.php');
+// 認証ページにリダイレクト
+$params = array(
+		'client_id' => APP_ID,
+		'redirect_uri' => CALLBACK_URL,
+		'response_type' => 'code', );
+header("Location: " . 'https://secure.freee.co.jp/oauth/authorize/'. '?' . http_build_query($params));
 
-header("Content-Type: text/html; charset=UTF-8");
-echo "{\"status\":\"OK\",\"message\":\"true\"}";
+
+
+
+
+//header("Content-Type: text/html; charset=UTF-8");
+//echo "{\"status\":\"OK\",\"message\":\"true\"}";
