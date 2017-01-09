0a3ffe2e91815c806c87db0631998c7fea4d9378
syun_alien <next.s.bdf@gmail.com>
2017-01-09 15:50:05 +0900

    不要サンプルを削除、php追加

--- js/Main.js
+++ js/Main.js
@@ -1,39 +1,76 @@
 /**
  *
  */
-(function () {
-        "use strict";
+(function() {
+	"use strict";
 
-        // The initialize function must be run each time a new page is loaded
-        Office.initialize = function (reason) {
-            $(document).ready(function () {
-                app.initialize();
+	// The initialize function must be run each time a new page is loaded
+	Office.initialize = function(reason) {
+		$(document).ready(function() {
+			app.initialize();
 
-                $('#getResult').click(getResult);
-            });
-        };
+			$('#getResult').click(getResult);
+		});
+	};
 
-        // Load some sample data into the worksheet and then create a chart
-        function getResult() {
-        	$("#result").text("通信成功");
+	// Load some sample data into the worksheet and then create a chart
+	function getResult() {
+		$("#result").text("通信成功");
 
+		var url = "https://freeephp.mybluemix.net/php/main.php";
 
+		var param = {
+			"request" : ""
+		};
 
+		/**
+		 * Ajax通信メソッド
+		 */
+		$
+				.ajax({
+					type : "GET",
+					url : url,
+					dataType : "jsonp",
+					jsonp : 'jsoncallback',
+					data : param,
+					/**
+					 * Ajax通信が成功した場合に呼び出されるメソッド
+					 */
+					success : function(data) {
+						$("#resultPhp").text(data + ": php通信成功");
+					},
 
+					/**
+					 * Ajax通信が失敗場合に呼び出されるメソッド
+					 */
+					error : function(XMLHttpRequest, textStatus, errorThrown) {
 
+						/*
+						 * 各エラーフィールドを設定
+						 */
+						$('#errorfield').append('<hr><p>エラーが発生しました</p>');
+						// リクエスト
+						$('#errorfield')
+								.append(
+										'<p>リクエスト</p><div id="request_url" ></div><div id="request_parameter" ></div>');
+						// レスポンス
+						$('#errorfield')
+								.append(
+										'<p>レスポンス</p><div id="response_parameter" ></div>');
+						// エラー
+						$('#errorfield')
+								.append(
+										'<p>エラー</p><div id="XMLHttpRequest" ></div><div id="textStatus" ></div><div id="errorThrown" ></div>');
 
+						// エラー情報を出力します。
+						errorDebug(XMLHttpRequest, textStatus, errorThrown);
 
+					}
+				});
 
-
-
-
-
-
-
-
-        /**
-         * サンプルのコード
-         */
+		/**
+		 * サンプルのコード
+		 */
 		// // Run a batch operation against the Excel object model
 		// Excel.run(function (ctx) {
 		//
@@ -90,7 +127,7 @@
 		// console.log("Error: " + error);
 		// if (error instanceof OfficeExtension.Error) {
 		// console.log("Debug info: " + JSON.stringify(error.debugInfo));
-		//                }
-		//            });
-        }
-    })();
+		// }
+		// });
+	}
+})();
