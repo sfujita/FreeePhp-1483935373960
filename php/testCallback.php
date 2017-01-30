f3de2014f793250259ea7029d60fe2ea4e8051ce
syun_alien <next.s.bdf@gmail.com>
2017-01-31 01:27:24 +0900

    testエラーのため修正

--- php/testCallback.php
+++ php/testCallback.php
@@ -2,14 +2,7 @@
 define ( 'APP_ID', 'c51200f7d63b84569dcb4b62b36d8e78b97a435fa2abd813398da29757e5efc6' );
 define ( 'APP_SECRET', 'c4f731db706aadc1445c956789a5c1546ec86f61e5395332c8c1ed65003e724b' );
 define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/testCallback.php' );
-// ※注意：APP_CALLBACKの値は、freeeのアプリケーション一覧に登録されている
-// コールバックURIと同じにする必要があります。
 
-// (1) いちばん最初の処理。OAuth2の入り口。
-// if (empty ( $_GET )) {
-// printf ( '<html><a href="%s?client_id=%s&redirect_uri=%s&response_type=code">認証開始</a></html>', 'https://secure.freee.co.jp/oauth/authorize', // 認証用
-// APP_ID, urlencode ( APP_CALLBACK ) );
-// }
 
 // (2) freeeで「許可する」が押されたあとに実行する処理
 if (! empty ( $_GET ['code'] )) {
@@ -38,7 +31,6 @@ if (! is_null ( $token ['access_token'] )) {
         ];
 
 	// POST処理
-	// ここに渡すパラメータを設定する
 	var_dump ( "POST処理" );
 	$data = '{
 		"company_id" : 809788,
@@ -51,7 +43,7 @@ if (! is_null ( $token ['access_token'] )) {
 		"tax_code" : 108,
 		"item_id" : 127358720,
 		"amount" : 88888,
-		"description" : "株式会社ジョインシップのコミット確認"
+		"description" : "株式会社ジョインシップ"
 		}
 		],
 		"payments" : [
--- php/testSample.php
+++ php/testSample.php
@@ -2,12 +2,10 @@
 define ( 'APP_ID', 'c51200f7d63b84569dcb4b62b36d8e78b97a435fa2abd813398da29757e5efc6' );
 define ( 'APP_SECRET', 'c4f731db706aadc1445c956789a5c1546ec86f61e5395332c8c1ed65003e724b' );
 define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/testCallback.php' );
-// ※注意：APP_CALLBACKの値は、freeeのアプリケーション一覧に登録されている
-// コールバックURIと同じにする必要があります。
 
 // (1) いちばん最初の処理。OAuth2の入り口。
 if (empty ( $_GET )) {
-	printf ( '<html><a href="%s?client_id=%s&redirect_uri=%s&response_type=code">GET認証開始</a></html>',
+	printf ( '<html><a href="%s?client_id=%s&redirect_uri=%s&response_type=code">テスト用認証開始</a></html>',
 			 'https://secure.freee.co.jp/oauth/authorize', // 認証用
 APP_ID, urlencode ( APP_CALLBACK ) );
 }
