5ebe26c34769f690b66bcf4db20a382e80661073
syun_alien <next.s.bdf@gmail.com>
2017-02-04 05:22:36 +0900

    ボタン処理修正

--- js/companies.js
+++ js/companies.js
@@ -1,58 +1,65 @@
 /**
  * 事業所IDを取得するjs。
  */
-$(document).ready(function()
-    {
+$(document).ready(function() {
 
-        /**
-         * 送信ボタンクリック
-         */
-        $('#companies').click(function()
-        {
 
-        	alert("ajax開始");
-            //POSTメソッドで送るデータを定義します var data = {パラメータ名 : 値};
-//            var data = {request : $('#request').val()};
-        	var data = ""; // ひとまず何も送らない
-            // 通信先php
-            var url = "https://freeephp.mybluemix.net/php/companies/companies.php";
 
-            /**
-             * Ajax通信メソッド
-             * @param type  : HTTP通信の種類
-             * @param url   : リクエスト送信先のURL
-             * @param data  : サーバに送信する値
-             */
-            $.ajax({
-                type: "GET",
-                url: url,
-                data: data,
-                /**
-                 * Ajax通信が成功した場合に呼び出されるメソッド
-                 */
-                success: function(data, dataType)
-                {
-                    //successのブロック内は、Ajax通信が成功した場合に呼び出される
 
-                    //PHPから返ってきたデータの表示
-                    alert(data);
-                },
-                /**
-                 * Ajax通信が失敗した場合に呼び出されるメソッド
-                 */
-                error: function(XMLHttpRequest, textStatus, errorThrown)
-                {
-                    //通常はここでtextStatusやerrorThrownの値を見て処理を切り分けるか、単純に通信に失敗した際の処理を記述します。
+	/**
+	 * 送信ボタンクリック
+	 */
+	$('#companies').click(function() {
 
-                    //this;
-                    //thisは他のコールバック関数同様にAJAX通信時のオプションを示します。
+		alert("ajax開始");
+		// POSTメソッドで送るデータを定義します var data = {パラメータ名 : 値};
+		// var data = {request : $('#request').val()};
+		var data = ""; // ひとまず何も送らない
+		// 通信先php
+		var url = "https://freeephp.mybluemix.net/php/companies/companies.php";
 
-                    //エラーメッセージの表示
-                    alert('Error : ' + errorThrown);
-                }
-            });
+		/**
+		 * Ajax通信メソッド
+		 *
+		 * @param type :
+		 *            HTTP通信の種類
+		 * @param url :
+		 *            リクエスト送信先のURL
+		 * @param data :
+		 *            サーバに送信する値
+		 */
+		$.ajax({
+			type : "GET",
+			url : url,
+			data : data,
+			/**
+			 * Ajax通信が成功した場合に呼び出されるメソッド
+			 */
+			success : function(data, dataType) {
+				// successのブロック内は、Ajax通信が成功した場合に呼び出される
 
-            //サブミット後、ページをリロードしないようにする
-            return false;
-        });
-    });
+				// PHPから返ってきたデータの表示
+				alert(data);
+			},
+			/**
+			 * Ajax通信が失敗した場合に呼び出されるメソッド
+			 */
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				// 通常はここでtextStatusやerrorThrownの値を見て処理を切り分けるか、単純に通信に失敗した際の処理を記述します。
+
+				// this;
+				// thisは他のコールバック関数同様にAJAX通信時のオプションを示します。
+
+				// エラーメッセージの表示
+				alert('Error : ' + errorThrown);
+			}
+		});
+
+		// サブミット後、ページをリロードしないようにする
+		return false;
+	});
+});
+
+function getToken(){
+	alert("getToken");
+}
