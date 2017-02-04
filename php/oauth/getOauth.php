7e708b0af13a559f2deb431ec2cf6199f1541f50
syun_alien <next.s.bdf@gmail.com>
2017-02-04 15:34:49 +0900

    php修正

--- php/oauth/getOauth.php
+++ php/oauth/getOauth.php
@@ -28,26 +28,28 @@ if (! empty ( $_GET ['code'] )) {
 	$jsonToken = curl_exec ( $curl );
 	$token = json_decode ( $jsonToken, true );
 
-	print<<<EOF
-	<!DOCTYPE html>
-		<html>
-		<head>
-		<meta charset="UTF-8">
-		<title>TOKEN取得</title>
-		<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-		<script type="text/javascript" src="js/companies.js"></script>
-		</head>
-		<body>
-			<div>TOKEN取得完了</div>
-			<br />
-			<div>TOKENは</div>
-			<br />
-			<div>$token</div>
+// 	print<<<EOF
+// 	<!DOCTYPE html>
+// 		<html>
+// 		<head>
+// 		<meta charset="UTF-8">
+// 		<title>TOKEN取得</title>
+// 		<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+// 		<script type="text/javascript" src="js/companies.js"></script>
+// 		</head>
+// 		<body>
+// 			<div>TOKEN取得完了</div>
+// 			<br />
+// 			<div>TOKENは</div>
+// 			<br />
+// 			<div>$token</div>
 
-		</body>
-		</html>
+// 		</body>
+// 		</html>
 
-	EOF;
+// 	EOF;
+
+	var_dump($token);
 
 }
 
