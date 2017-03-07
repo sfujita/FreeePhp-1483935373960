2364a77dbf2834aa81fb2a1bf1e3ee0651918372
syun_alien <next.s.bdf@gmail.com>
2017-03-08 00:48:43 +0900

    エラー処理追加

--- php/callback.php
+++ php/callback.php
@@ -1,4 +1,6 @@
 <?php
+ini_set("display_errors", On);
+error_reporting(E_ALL);
 define ( 'APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b' );
 define ( 'APP_SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841' );
 define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/callback.php' );
