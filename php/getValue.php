e8d9100205b169b8b8078b740206927c15db9363
syun_alien <next.s.bdf@gmail.com>
2017-05-12 01:31:21 +0900

    サンプル通りにphpを修正

--- php/getValue.php
+++ php/getValue.php
@@ -1,6 +1,14 @@
 <?php
+//ajax送信でPOSTされたデータを受け取る
+$post_data_1 = $_POST['post_data_1'];
+$post_data_2 = $_POST['post_data_2'];
 
+//受け取ったデータを配列に格納
+$return_array = array($post_data_1, $post_data_2);
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type:text/plain;charset=UTF-8');
-echo "successしました";
+//ヘッダーの設定
+header("Content-type:application/json; charset=utf8");
+header("Access-Control-Allow-Origin: *");
+
+//「$return_array」をjson_encodeして出力
+echo json_encode($return_array);
