60f20fb1413b35fcd4c53a5485e0140acbe98256
syun_alien <next.s.bdf@gmail.com>
2017-05-16 04:17:04 +0900

    結果の持たせ方を変更

--- php/getValue.php
+++ php/getValue.php
@@ -9,4 +9,4 @@ header("Access-Control-Allow-Origin: *");
 
 $result = file_get_contents('https://secure.freee.co.jp/oauth/authorize?client_id=f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b&redirect_uri=https%3A%2F%2Ffreeephp.mybluemix.net%2Fphp%2Fcallback.php&response_type=code');
 
-echo json_encode("{ test: \"$result\" }");
+echo json_encode($result);
