143f8559df1479a34eda75c2c642585f0f09e9d5
syun_alien <next.s.bdf@gmail.com>
2017-05-16 04:36:53 +0900

    ヘッダーを修正

--- php/getValue.php
+++ php/getValue.php
@@ -1,7 +1,7 @@
 <?php
 //ヘッダーの設定
-// header("Content-type:application/json; charset=utf8");
-// header("Access-Control-Allow-Origin: *");
+header("Content-type:application/json; charset=utf8");
+header("Access-Control-Allow-Origin: *");
 
 // $result = file_get_contents('https://secure.freee.co.jp/oauth/authorize?client_id=f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b&redirect_uri=https%3A%2F%2Ffreeephp.mybluemix.net%2Fphp%2Fcallback.php&response_type=code');
 
