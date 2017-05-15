e711c8ab59aff497e5c88727087abc9b77def790
syun_alien <next.s.bdf@gmail.com>
2017-05-15 23:57:57 +0900

    戻り値をphp側で指定

--- php/getValue.php
+++ php/getValue.php
@@ -4,11 +4,15 @@ $post_data_1 = $_POST['post_data_1'];
 $post_data_2 = $_POST['post_data_2'];
 
 //受け取ったデータを配列に格納
-$return_array = array($post_data_1, $post_data_2);
+// $return_array = array($post_data_1, $post_data_2);
+
+
 
 //ヘッダーの設定
 header("Content-type:application/json; charset=utf8");
 header("Access-Control-Allow-Origin: *");
 
 //「$return_array」をjson_encodeして出力
-echo json_encode($return_array);
+// echo json_encode($return_array);
+
+echo json_encode("{ test: \"ミトコンドリア\" }");
