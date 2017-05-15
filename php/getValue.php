0964c8ef04320d983ec0a233c1d50e8744486e6c
syun_alien <next.s.bdf@gmail.com>
2017-05-16 01:14:24 +0900

    認証用コード取得に変更

--- php/getValue.php
+++ php/getValue.php
@@ -3,16 +3,10 @@
 $post_data_1 = $_POST['post_data_1'];
 $post_data_2 = $_POST['post_data_2'];
 
-//受け取ったデータを配列に格納
-// $return_array = array($post_data_1, $post_data_2);
-
-
-
 //ヘッダーの設定
 header("Content-type:application/json; charset=utf8");
 header("Access-Control-Allow-Origin: *");
 
-//「$return_array」をjson_encodeして出力
-// echo json_encode($return_array);
+$result = file_get_contents('https://secure.freee.co.jp/oauth/authorize?client_id=f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b&redirect_uri=https%3A%2F%2Ffreeephp.mybluemix.net%2Fphp%2Fcallback.php&response_type=code');
 
-echo json_encode("{ test: \"ミトコンドリア\" }");
+echo json_encode("{ test: \"$result\" }");
