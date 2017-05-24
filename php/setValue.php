7419dcc1de5428e07c30261559ff39a03a6b27ab
syun_alien <next.s.bdf@gmail.com>
2017-05-25 02:19:28 +0900

    ファイルの読み込みを修正

--- php/setValue.php
+++ php/setValue.php
@@ -2,12 +2,19 @@
 
 // ファイルのパスを変数に格納
 $filename = 'param.txt';
+
+$content = "";
+
+
 // 引数の金額を格納
 // $param = $_POST['param'];
 
 // ファイルの存在をチェックし、存在する場合は削除する
 if (file_exists ( $filename )) {
 	unlink ( $filename );
+
+	$content = $content."ファイルを削除しました";
+
 }
 
 $param = "これはテスト";
@@ -15,7 +22,11 @@ $param = "これはテスト";
 file_put_contents ( $filename, $param );
 
 // ファイルを読み込み変数に格納
-$content = file_get_contents ( $filename );
+$content2 = file_get_contents ( $filename );
 
-header ( 'Content-Type: application/json; charset=utf-8' );
-echo json_encode ( $content );
+$content3 = $content.$content2;
+
+
+
+header("Content-type: text/plain; charset=UTF-8");
+echo $content3;
