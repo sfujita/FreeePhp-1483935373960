c31e36c8a2d8ab8b20687d85396c4727e19cf506
syun_alien <next.s.bdf@gmail.com>
2017-05-27 15:52:14 +0900

    一旦戻す

--- php/setValue.php
+++ php/setValue.php
@@ -54,9 +54,9 @@ $jsonArray = array(
 		),
 );
 header('Content-Type: text/javascript; charset=utf-8');
-// echo sprintf("callback(%s)",json_encode($jsonArray));
+echo sprintf("callback(%s)",json_encode($jsonArray));
 
-echo sprintf($_GET["callback"]."(".json_encode($jsonArray).")"); ?
+// echo sprintf($_GET["callback"]."(".json_encode($jsonArray).")"); ?
 
 // echo $_GET["callback"]."(".json_encode($jsonArray).")"; ?
 
