eb461d0324bbb6f3424ef6f1ccdea921fe453fca
syun_alien <next.s.bdf@gmail.com>
2017-05-27 13:12:25 +0900

    サンプルデータに置換

--- php/setValue.php
+++ php/setValue.php
@@ -1,27 +1,59 @@
 <?php
 
 // ファイルのパスを変数に格納
-$filename = 'param.txt';
+// $filename = 'param.txt';
 
-$content = "";
+// $content = "";
 
 
-// 引数の金額を格納
-// $param = $_POST['param'];
+// // 引数の金額を格納
+// // $param = $_POST['param'];
 
-// ファイルの存在をチェックし、存在する場合は削除する
-if (file_exists ( $filename )) {
-	unlink ( $filename );
+// // ファイルの存在をチェックし、存在する場合は削除する
+// if (file_exists ( $filename )) {
+// 	unlink ( $filename );
 
-	$content = $content."ファイルを削除しました";
+// 	$content = $content."ファイルを削除しました";
 
-}
+// }
 
-$text = filter_input(INPUT_GET, 'text');
-$callback = filter_input(INPUT_GET, 'callback');
-$callback = htmlspecialchars(strip_tags($callback));
+// $text = filter_input(INPUT_GET, 'text');
+// $callback = filter_input(INPUT_GET, 'callback');
+// $callback = htmlspecialchars(strip_tags($callback));
 
-$param = ['text' => $text . ", World!"];
+// $param = ['text' => $text . ", World!"];
 
-header('Content-type: text/javascript; charset=utf-8');
-printf("{$callback}(%s)", json_encode( $param ));
+// header('Content-type: text/javascript; charset=utf-8');
+// printf("{$callback}(%s)", json_encode( $param ));
+
+
+
+$jsonArray = array(
+		array(
+				'title'       => 'テストデータ１タイトル',
+				'description' => 'テストデータ１概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ２タイトル',
+				'description' => 'テストデータ２概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ３タイトル',
+				'description' => 'テストデータ３概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ４タイトル',
+				'description' => 'テストデータ４概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ５タイトル',
+				'description' => 'テストデータ５概要',
+				'url' => 'http://www.google.com'
+		),
+);
+header('Content-Type: text/javascript; charset=utf-8');
+echo sprintf("callback(%s)",json_encode($jsonArray));
