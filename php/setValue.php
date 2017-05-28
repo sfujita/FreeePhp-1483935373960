2ef632e09ae0f622ef9e298afb778eea97476e69
syun_alien <next.s.bdf@gmail.com>
2017-05-28 12:18:01 +0900

    ★動作確認ポイント①

--- php/setValue.php
+++ php/setValue.php
@@ -1,6 +1,12 @@
 <?php
 
-// jsonのコールバックを設定する
+
+
+// $param = ['text' => $text . ", World!"];
+
+// header('Content-type: text/javascript; charset=utf-8');
+// printf("{$callback}(%s)", json_encode( $param ));
+
 $callback = "jsonCallback";
 if(isset($_GET['callback'])){
 	$callback=$_GET['callback'];
@@ -10,32 +16,32 @@ if(isset($_GET['callback'])){
 // ファイルのパスを変数に格納
 $filename = 'param.txt';
 
-// 返却用ステータスを初期化する
-$status = "0";
+// $content = "";
 
 
 // // 引数の金額を格納
-// $param = $_GET['param'];
+// // $param = $_POST['param'];
 
 // // ファイルの存在をチェックし、存在する場合は削除する
 // if (file_exists ( $filename )) {
 // 	unlink ( $filename );
 
-// 	// ステータスを"1"：削除に変更する
-// 	$status = "1";
+// 	$content = $content."ファイルを削除しました";
 
 // }
 
-// 引数の金額をテキストファイルに書き込む
-// $text = filter_input(INPUT_GET, $param);
-
+// $text = filter_input(INPUT_GET, 'text');
+// $callback = filter_input(INPUT_GET, 'callback');
+// $callback = htmlspecialchars(strip_tags($callback));
 
 
 
 $jsonArray = array(
-// 		array(
-// 				'status'       => $status
-// 		),
+		array(
+				'title'       => 'テストデータ１タイトル',
+				'description' => 'テストデータ１概要',
+				'url' => 'http://www.google.com'
+		),
 		array(
 				'title'       => 'テストデータ２タイトル',
 				'description' => 'テストデータ２概要',
@@ -57,10 +63,13 @@ $jsonArray = array(
 				'url' => 'http://www.google.com'
 		),
 );
-
 header('Content-Type: text/javascript; charset=utf-8');
+// echo sprintf("callback(%s)",json_encode($jsonArray));
+
 $json = json_encode($jsonArray);
+
 
 print <<<END
 $callback($json);
 END;
+
