c70f1d693f617555d0d63a25a287683b912b5bb8
syun_alien <next.s.bdf@gmail.com>
2017-05-30 01:13:25 +0900

    ファイル作成処理を実装

--- php/setValue.php
+++ php/setValue.php
@@ -2,12 +2,11 @@
 
 
 
-// $param = ['text' => $text . ", World!"];
-
-// header('Content-type: text/javascript; charset=utf-8');
-// printf("{$callback}(%s)", json_encode( $param ));
-
+// 引数の金額を格納
+$param = $_GET['param'];
 $callback = "jsonCallback";
+
+// コールバックを設定する
 if(isset($_GET['callback'])){
 	$callback=$_GET['callback'];
 }
@@ -16,51 +15,30 @@ if(isset($_GET['callback'])){
 // ファイルのパスを変数に格納
 $filename = 'param.txt';
 
-// $content = "";
+// ファイルのステータスを初期化する
+// "0"：新規作成
+// "1"：削除&作成
+$fileStatus = "0";
 
 
-// 引数の金額を格納
-$param = $_GET['param'];
-
-// // ファイルの存在をチェックし、存在する場合は削除する
-// if (file_exists ( $filename )) {
-// 	unlink ( $filename );
-
-// 	$content = $content."ファイルを削除しました";
-
-// }
-
-// $text = filter_input(INPUT_GET, 'text');
-// $callback = filter_input(INPUT_GET, 'callback');
-// $callback = htmlspecialchars(strip_tags($callback));
 
 
+// ファイルの存在をチェックし、存在する場合は削除する
+if (file_exists ( $filename )) {
+	unlink ( $filename );
+
+	// 削除&作成
+	$fileStatus = "1";
+
+}
+
+// ファイルに引数の金額を格納する
+file_put_contents($filename,$param)
 
 $jsonArray = array(
 		array(
-				'param'       => $param,
-				'description' => 'テストデータ１概要',
-				'url' => 'http://www.google.com'
-		),
-		array(
-				'title'       => 'テストデータ２タイトル',
-				'description' => 'テストデータ２概要',
-				'url' => 'http://www.google.com'
-		),
-		array(
-				'title'       => 'テストデータ３タイトル',
-				'description' => 'テストデータ３概要',
-				'url' => 'http://www.google.com'
-		),
-		array(
-				'title'       => 'テストデータ４タイトル',
-				'description' => 'テストデータ４概要',
-				'url' => 'http://www.google.com'
-		),
-		array(
-				'title'       => 'テストデータ５タイトル',
-				'description' => 'テストデータ５概要',
-				'url' => 'http://www.google.com'
+				'status' => $fileStatus,
+				'param'  => $param,
 		),
 );
 header('Content-Type: text/javascript; charset=utf-8');
