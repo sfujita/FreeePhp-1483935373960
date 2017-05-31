6f8a04fab0af08e833842f227e230a99a88afcf7
syun_alien <next.s.bdf@gmail.com>
2017-06-01 01:01:12 +0900

    ファイル作成処理はコメント

--- php/setValue.php
+++ php/setValue.php
@@ -13,7 +13,7 @@ if(isset($_GET['callback'])){
 
 
 // ファイルのパスを変数に格納
-$filename = 'param.txt';
+$fileName = 'param.txt';
 
 // ファイルのステータスを初期化する
 // "0"：新規作成
@@ -23,22 +23,43 @@ $fileStatus = "0";
 
 
 
-// ファイルの存在をチェックし、存在する場合は削除する
-if (file_exists ( $filename )) {
-	unlink ( $filename );
+// // ファイルの存在をチェックし、存在する場合は削除する
+// if (file_exists ( $filename )) {
+// 	unlink ( $filename );
 
-	// 削除&作成
-	$fileStatus = "1";
+// 	// 削除&作成
+// 	$fileStatus = "1";
 
-}
+// }
 
-// ファイルに引数の金額を格納する
-file_put_contents($filename,$param)
+// // ファイルに引数の金額をかく
+// file_put_contents($filename,$param)
 
 $jsonArray = array(
 		array(
-				'status' => $fileStatus,
-				'param'  => $param,
+				'param'       => $param,
+				'fileName' => $fileName,
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ２タイトル',
+				'description' => 'テストデータ２概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ３タイトル',
+				'description' => 'テストデータ３概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ４タイトル',
+				'description' => 'テストデータ４概要',
+				'url' => 'http://www.google.com'
+		),
+		array(
+				'title'       => 'テストデータ５タイトル',
+				'description' => 'テストデータ５概要',
+				'url' => 'http://www.google.com'
 		),
 );
 header('Content-Type: text/javascript; charset=utf-8');
