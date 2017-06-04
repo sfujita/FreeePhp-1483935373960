ceb251d566f0a08b4d10abe8ba0eeb7c51cf8c0c
syun_alien <next.s.bdf@gmail.com>
2017-06-04 10:29:03 +0900

    読み込みまでを確認

--- php/callback.php
+++ php/callback.php
@@ -1,6 +1,6 @@
 <?php
-ini_set("display_errors", On);
-error_reporting(E_ALL);
+ini_set ( "display_errors", On );
+error_reporting ( E_ALL );
 define ( 'APP_ID', 'f59339c44c93cdf7598607005ddea573a200cfc23f10d5954c03e98cf027958b' );
 define ( 'APP_SECRET', '7849ea17ddb74e188afda1087eae8aac9e7fa94111e89f215e816dc801cd7841' );
 define ( 'APP_CALLBACK', 'https://freeephp.mybluemix.net/php/callback.php' );
@@ -30,7 +30,7 @@ if (! empty ( $_GET ['code'] )) {
 	$jsonToken = curl_exec ( $curl );
 	$token = json_decode ( $jsonToken, true );
 
-// 	var_dump ( $token );
+	// var_dump ( $token );
 }
 
 // (3) Token取得後の処理。各種APIの実行
@@ -39,24 +39,104 @@ if (! is_null ( $token ['access_token'] )) {
             'Authorization: Bearer ' . $token['access_token'],
         ];
 
-	// ↓↓↓↓↓↓↓
-	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報（org）
-	                                                                               // $curl = curl_init ( 'https://api.freee.co.jp/api/1/account_items?company_id=809788' ); // 勘定科目一覧の取得
-	                                                                               // $curl = curl_init ( 'https://api.freee.co.jp/api/1/deals?company_id=809788' ); // 取引（収入／支出）一覧の取得
+	// ファイルのパスを変数に格納
+	$fileName = './param.txt';
 
-	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
-	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
-	$jsonResult = curl_exec ( $curl );
-	$result = json_decode ( $jsonResult, true );
+	// ファイルを読み込み専用でオープンする
+	$fp = fopen ( $fileName, 'r' );
 
-	header ( "Content-Type:text/html; charset=UTF-8" );
+	// 読み込むファイルが存在しない場合、エラーとして処理終了
+	if ($fp == false) {
+		echo "エラーが発生しました[読み込みファイルが存在しません]";
+		return;
+	}
 
-	var_dump ( "取得した会社名<br />" );
-	var_dump ( $result ["user"] ["companies"] [0] ["display_name"] . "<br />" );
-	var_dump ( "企業コード<br />" );
-	var_dump ( $result ["user"] ["companies"] [0] ["id"] . "<br />" );
+	// ファイルから一行読み込む
+	$line = fgets ( $fp );
+	// 読み込んだ行を変数に格納する
+	$value = $line;
+	// ファイルをクローズする
+	fclose ( $fp );
 
-};
+	var_dump ($value);
+
+// 	// ↓↓↓↓↓↓↓
+// 	$curl = curl_init ( 'https://api.freee.co.jp/api/1/users/me?companies=true' ); // 自分の情報（org）
+// 	                                                                               // $curl = curl_init ( 'https://api.freee.co.jp/api/1/account_items?company_id=809788' ); // 勘定科目一覧の取得
+// 	                                                                               // $curl = curl_init ( 'https://api.freee.co.jp/api/1/deals?company_id=809788' ); // 取引（収入／支出）一覧の取得
+
+// 	curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+// 	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+// 	$jsonResult = curl_exec ( $curl );
+// 	$result = json_decode ( $jsonResult, true );
+
+// 	header ( "Content-Type:text/html; charset=UTF-8" );
+
+// 	var_dump ( "取得した会社名<br />" );
+// 	var_dump ( $result ["user"] ["companies"] [0] ["display_name"] . "<br />" );
+// 	var_dump ( "企業コード<br />" );
+// 	var_dump ( $result ["user"] ["companies"] [0] ["id"] . "<br />" );
+
+// 	//
+// 	// 登録まで行けるかを確認
+// 	//
+// 	$data = '{
+// 			"company_id" : 809788,
+// 			"issue_date" : "2017-01-19",
+// 			"due_date" : "2017-02-28",
+// 			"type" : "income",
+// 			"details" : [
+// 			{
+// 			"account_item_id" : 127717210,
+// 			"tax_code" : 108,
+// 			"item_id" : 127358720,
+// 			"amount" : 88888,
+// 			"description" : "株式会社ジョインシップ"
+// 			}
+// 			],
+// 			"payments" : [
+// 			{
+// 			"date" : "2017-01-28",
+// 			"from_walletable_type" : "credit_card",
+// 			"from_walletable_id" : 150980,
+// 			"amount" : 66666
+// 			}
+// 		 ]
+// 		}';
+
+// 	$url = 'https://api.freee.co.jp/api/1/deals';
+
+// 	$options = array (
+// 			CURLOPT_RETURNTRANSFER => true,
+// 			CURLOPT_FOLLOWLOCATION => true,
+// 			CURLOPT_AUTOREFERER => true
+// 	);
+// 	$ch = curl_init ();
+// 	curl_setopt ( $ch, CURLOPT_URL, $url );
+
+// 	$header = [
+// 			'Authorization: Bearer ' . $token,
+// 	];
+
+// 	curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+// 			'Content-Type: application/json',
+// 			'Authorization: Bearer ' . $token
+// 	) );
+// 	// curl_setopt ( $ch, CURLOPT_HTTPHEADER, $header );
+
+// 	curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );
+// 	curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false );
+// 	curl_setopt ( $ch, CURLOPT_VERBOSE, true );
+// 	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+// 	curl_setopt ( $ch, CURLOPT_POST, true );
+// 	curl_setopt_array ( $ch, $options );
+// 	$result = curl_exec ( $ch );
+// 	curl_close ( $ch );
+
+// 	var_dump ( "取得した情報" );
+// 	var_dump ( $result );
+}
+;
 
 // 	// ※※※※※※※※※※※　勘定科目取得処理開始　※※※※※※※※※※※
 
